5e4bf7597392097ba4adcc17f25a4aa03f0e2064
ginabeki <ginabeki3@gmail.com>
2022-12-22 13:06:16 +0300

    Update the store

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -2,33 +2,63 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 // ACTIONS
-const LOAD = 'space-travellers/missions/LOAD';
+const GET_MISSIONS = 'space-travelers-hub/missions/GET_MISSIONS';
+const JOIN_MISSION = 'space-travelers-hub/missions/JOIN_MISSION';
+const LEAVE_MISSION = 'space-travelers-hub/missions/LEAVE_MISSION';
 
 const initialState = {
   missions: [],
 };
 
 const missionsAPI = 'https://api.spacexdata.com/v3/missions';
+export const LoadMissions = createAsyncThunk(GET_MISSIONS, async () => {
+  const response = await axios.get(missionsAPI);
+  const missions = Object.keys(response.data).map((key) => ({
+    id: response.data[key].mission_id,
+    missionName: response.data[key].mission_name,
+    description: response.data[key].description,
+    reserved: false,
+  }));
+  return missions;
+});
 
 export function missionsReducer(state = initialState, action = {}) {
   switch (action.type) {
-    case `${LOAD}/fulfilled`:
+    case `${GET_MISSIONS}/fulfilled`:
       return {
-        missions: [action.payload],
+        ...state,
+        missions: action.payload,
+      };
+    case JOIN_MISSION:
+      return {
+        ...state,
+        missions: state.missions.map((mission) => {
+          if (mission.mission_id !== action.id) {
+            return { ...mission };
+          }
+          return { ...mission, reserved: true };
+        }),
+      };
+    case LEAVE_MISSION:
+      return {
+        ...state,
+        missions: state.missions.map((mission) => {
+          if (mission.mission_id !== action.id) {
+            return { ...mission };
+          }
+          return { ...mission, reserved: false };
+        }),
       };
     default:
       return state;
   }
 }
 
-export const LoadMissions = createAsyncThunk(LOAD, async () => {
-  const response = await axios.get(missionsAPI);
-  const missions = Object.keys(response.data).map((key) => ({
-    id: response.data[key].mission_id,
-    missionName: response.data[key].mission_name,
-    description: response.data[key].description,
-  }));
-  return missions;
+export const joinMission = (payload) => ({ type: JOIN_MISSION, id: payload });
+
+export const leaveMission = (payload) => ({
+  type: LEAVE_MISSION,
+  id: payload,
 });
 
 export default { missionsReducer, LoadMissions };
